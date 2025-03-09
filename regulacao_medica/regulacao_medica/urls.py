6ee3778cf96ac769bc1d9ca7733e46fc223d2f42
dirceuvaz <dirceuvaz.dev@gmail.com>
2025-03-09 16:17:40 -0300

    Limpando a branch

--- regulacao_medica/regulacao_medica/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('oauth/', include('social_django.urls', namespace='social')),
-    path('', include('core.urls')),
-    path('accounts/', include('django.contrib.auth.urls')),
-]
--- regulacao_medica/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/users/', include('core.urls')),  # Inclua as URLs da sua aplicação
-    path('admin/', admin.site.urls),  # URLs do sistema de administração do Django
-    path('', include('core.urls')),  # Inclua as URLs da sua aplicação principal
-]
